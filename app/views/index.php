22af634a2444e658e50c584c039d960a62a917c0
Skyseras <yasserelmahfoudi@gmail.com>
2022-06-01 19:41:20 +0100

    home page

---
+++ app/views/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>ImmoMaroc</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="preconnect" href="https://fonts.gstatic.com/">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700&amp;display=swap">
+	<link rel="stylesheet" href="css/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/ajax/libs/ionicons/4.5.6/css/ionicons.min.css">
+	<link rel="stylesheet" href="css/A.animate.css%2bflaticon.css%2btiny-slider.css%2bglightbox.min.css%2baos.css%2bstyle.css%2cMcc.GVIUmIpB3f.css.pagespeed.cf.wuDyiIMmfX.css" />
+</head>
+
+<body>
+	<div class="py-3 top-wrap">
+		<div class="container-xl">
+			<div class="row d-flex align-items-start">
+				<div class="col-md topper d-flex mb-md-0 align-items-xl-center">
+					<div class="icon d-flex justify-content-center align-items-center"><span class="fa fa-map"></span>
+					</div>
+					<div class="text pl-3 pl-md-3">
+						<p class="con"><span>Appel gratuit</span> <span>+212 708 884 824</span></p>
+						<p class="con">Appelez-nous maintenant Service client 24h/7j</p>
+					</div>
+				</div>
+				<div class="col-md topper d-flex mb-md-0 align-items-xl-center">
+					<div class="icon d-flex justify-content-center align-items-center"><span class="fa fa-paper-plane"></span>
+					</div>
+					<div class="text pl-3 pl-md-3">
+						<p class="hr"><span>Notre emplacement</span></p>
+						<p class="con">Bureau 7 Jozefine Center Majorelle Marrakech</p>
+					</div>
+				</div>
+				<div class="col-md topper d-flex mb-md-0 align-items-xl-center">
+					<div class="icon d-flex justify-content-center align-items-center"><span class="fa fa-connectdevelop"></span>
+					</div>
+					<div class="text pl-3 pl-md-3">
+						<p class="con"><span>Connectez -vous </span> <span>avec nous</span></p>
+						<p class="con"><a href="#">Facebook</a> <a href="#">Twitter</a> <a href="#">Instagram</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-expand-lg  ftco-navbar-light">
+		<div class="container-xl">
+			<a class="navbar-brand align-items-center" href="index.php" style="width: 20%;">
+				<img src="images/Logo_IM_L_W.png" alt="logo" class="img-fluid">
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fa fa-bars"></span> Menu
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav m-auto mb-2 mb-lg-0">
+					<li class="nav-item"><a class="nav-link" href="index-2.html">ACCUEIL</a></li>
+					<li class="nav-item"><a class="nav-link" href="properties.html">PROPRIÉTÉS</a></li>
+					<li class="nav-item"><a class="nav-link" href="agents.html">AGENTS</a></li>
+					<li class="nav-item"><a class="nav-link" href="blog.html">BLOG</a></li>
+					<li class="nav-item"><a class="nav-link" href="contact.html">CONTACT</a></li>
+				</ul>
+				<p class="mb-0"><a href="#" class="btn btn-primary rounded">Soumettre une propriété</a></p>
+			</div>
+		</div>
+	</nav>
+	<section class="slider-hero">
+		<div class="overlay"></div>
+		<div class="hero-slider">
+			<div class="item" style="height:80vh;">
+				<div class="work">
+					<div class="img d-flex align-items-center js-fullheight" style="background-image:url(images/xbg_1.jpg.pagespeed.ic.b31fPYrhFV.jpg)">
+						<div class="container-xl">
+							<div class="row justify-content-center align-items-center">
+								<div class="col-md-10 col-xl-9">
+									<div class="text text-center" data-aos="fade-up" data-aos-duration="1000">
+										<h2>Votre Propriété Est Notre Priorité.</h2>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item" style="height:80vh;">
+				<div class="work">
+					<div class="img d-flex align-items-center justify-content-center js-fullheight" style="background-image:url(images/xbg_2.jpg.pagespeed.ic.4lj8Kq4iRv.jpg)">
+						<div class="container-xl">
+							<div class="row justify-content-center">
+								<div class="col-md-10 col-xl-9">
+									<div class="text text-center" data-aos="fade-up" data-aos-duration="1000">
+										<h2>Des Professionnels De L'immobilier À Votre Écoute.</h2>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item" style="height:80vh;">
+				<div class="work">
+					<div class="img d-flex align-items-center justify-content-center js-fullheight" style="background-image:url(images/xbg_3.jpg.pagespeed.ic.33WGlYurvU.jpg)">
+						<div class="container-xl">
+							<div class="row justify-content-center">
+								<div class="col-md-10 col-xl-9">
+									<div class="text text-center" data-aos="fade-up" data-aos-duration="1000">
+										<h2>Trouvez Votre Maison De Rêve.</h2>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="js/tiny-slider.js"></script>
+	<script src="js/glightbox.min.js%2baos.js%2bgoogle-map.js%2bmain.js.pagespeed.jc.Ed1LceD4Zh.js"></script>
+	<script>
+		eval(mod_pagespeed_rdWE5e7ZHS);
+	</script>
+	<script>
+		eval(mod_pagespeed_TbXghfTMVh);
+	</script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&amp;sensor=false"></script>
+	<script>
+		eval(mod_pagespeed_Jq3FPUXGcI);
+	</script>
+	<script>
+		eval(mod_pagespeed_F7TkqtYUhP);
+	</script>
+
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-23581568-13');
+	</script>
+	<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"7140bba3fee3c724","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2021.12.0","si":100}' crossorigin="anonymous"></script>
+</body>
+
+</html>
