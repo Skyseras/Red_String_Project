1fcb837f46d3249683d178336b43fc37de5873e2
Skyseras <yasserelmahfoudi@gmail.com>
2022-06-03 10:17:55 +0100

    Includes added

---
+++ app/views/includes/footer.php
@@ -0,0 +1,106 @@
+<footer class="ftco-footer">
+    <div class="container-xl">
+        <div class="row mb-5 pb-5 justify-content-between">
+            <div class="col-md-6 col-lg">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2 logo d-flex">
+                        <a class="navbar-brand align-items-center" href="index-2.html" style="width: 65%;">
+                            <img src="images/Logo_IM_L_W.png" alt="logo" class="img-fluid">
+                        </a>
+                    </h2>
+                    <ul class="ftco-footer-social list-unstyled mt-2">
+                        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                        <li><a href="#"><span class="fa fa-instagram"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-2">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Offres</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Propriétés</a></li>
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Agents</a></li>
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Emplacements</a></li>
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Assistance Clients</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-2">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Entreprise</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Home</a></li>
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>À propos de nous</a></li>
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Blog</a></li>
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contactez-nous</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-2">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Liens rapides</h2>
+                    <ul class="list-unstyled">
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Termes&amp;Conditions</a></li>
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Guide de l'utilisateur</a></li>
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Support Center</a></li>
+                        <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Info presse</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Vous avez une question ?</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li><span class="icon fa fa-map marker"></span><span class="text">Bureau 7 Jozefine Centre Majorelle Marrakech</span></li>
+                            <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+212 708 884 824</span></a></li>
+                            <li><a href="#"><span class="icon fa fa-paper-plane pr-4"></span><span class="text"><span class="__cf_email__" data-cfemail="422b2c242d023b2d3730262d2f232b2c6c212d2f">[info@immomaroc.ma, email&#160;protected]</span></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid px-0 py-5 bg-darken">
+        <div class="container-xl">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p class="mb-0" style="color: rgba(255,255,255,.5); font-size: 13px;">Copyright &copy;<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | Powered with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="#" target="_blank" rel="nofollow noopener">ImmoMaroc</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/tiny-slider.js"></script>
+<script src="js/glightbox.min.js%2baos.js%2bgoogle-map.js%2bmain.js.pagespeed.jc.Ed1LceD4Zh.js"></script>
+<script>
+    eval(mod_pagespeed_rdWE5e7ZHS);
+</script>
+<script>
+    eval(mod_pagespeed_TbXghfTMVh);
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&amp;sensor=false"></script>
+<script>
+    eval(mod_pagespeed_Jq3FPUXGcI);
+</script>
+<script>
+    eval(mod_pagespeed_F7TkqtYUhP);
+</script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-23581568-13');
+</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"7140bbb77d91c728","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2021.12.0","si":100}' crossorigin="anonymous"></script>
---
+++ app/views/includes/header.php
@@ -0,0 +1,50 @@
+<div class="py-3 top-wrap">
+    <div class="container-xl">
+        <div class="row d-flex align-items-start">
+            <div class="col-md topper d-flex mb-md-0 align-items-xl-center">
+                <div class="icon d-flex justify-content-center align-items-center"><span class="fa fa-map"></span>
+                </div>
+                <div class="text pl-3 pl-md-3">
+                    <p class="con"><span>Appel gratuit</span> <span>+212 708 884 824</span></p>
+                    <p class="con">Appelez-nous maintenant Service client 24h/7j</p>
+                </div>
+            </div>
+            <div class="col-md topper d-flex mb-md-0 align-items-xl-center">
+                <div class="icon d-flex justify-content-center align-items-center"><span class="fa fa-paper-plane"></span>
+                </div>
+                <div class="text pl-3 pl-md-3">
+                    <p class="hr"><span>Notre emplacement</span></p>
+                    <p class="con">Bureau 7 Jozefine Center Majorelle Marrakech</p>
+                </div>
+            </div>
+            <div class="col-md topper d-flex mb-md-0 align-items-xl-center">
+                <div class="icon d-flex justify-content-center align-items-center"><span class="fa fa-connectdevelop"></span>
+                </div>
+                <div class="text pl-3 pl-md-3">
+                    <p class="con"><span>Connectez-vous </span> <span>avec nous</span></p>
+                    <p class="con"><a href="#">Facebook</a> <a href="#">Twitter</a> <a href="#">Instagram</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<nav class="navbar navbar-expand-lg  ftco-navbar-light">
+    <div class="container-xl">
+        <a class="navbar-brand align-items-center" href="index.php" style="width: 20%;">
+            <img src="images/Logo_IM_L_W.png" alt="logo" class="img-fluid">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="fa fa-bars"></span> Menu
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link" href="index.php">ACCUEIL</a></li>
+                <li class="nav-item"><a class="nav-link" href="properties.html">PROPRIÉTÉS</a></li>
+                <li class="nav-item"><a class="nav-link" href="agents.html">AGENTS</a></li>
+                <li class="nav-item"><a class="nav-link" href="blog.html">BLOG</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">CONTACT</a></li>
+            </ul>
+            <p class="mb-0"><a href="#" class="btn btn-primary rounded">Soumettre une propriété</a></p>
+        </div>
+    </div>
+</nav>
